a3ba4e4e7ef18c4ac3f8207b833955b69d9e297c
Markswell <osoromarkswell80@gmail.com>
2023-11-21 11:08:34 +0300

    Assignment Complete

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0 :
+        print(counter)
+        counter - 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return list(map(lambda num: num ** 2, int_list))
 
-def fizzbuzz():
-    # code goes here!
-    pass
+def fizzbuzz_printer():
+    for num in range(1, 101):
+        print(fizzbuzz(num))
+
+def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+    
+def reverse_string(input_str):
+    reversed_str = ""
+    for char in input_str:
+        reversed_str = char + reversed_str
+    return reversed_str
